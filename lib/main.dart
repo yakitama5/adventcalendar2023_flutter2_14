8b1e7cea5ab2c9a22d44f99f1733729fdb03c1df
rittitarou@yahoo.co.jp <rittitarou@yahoo.co.jp>
2023-12-13 13:14:58 +0900

    🔧chore: URLから"#"を除外するように設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 import 'app/app.dart';
 
-void main() => runApp(const AdaptiveApp());
+void main() {
+  // WebのURLから "#" を削除
+  usePathUrlStrategy();
+
+  runApp(const AdaptiveApp());
+}
